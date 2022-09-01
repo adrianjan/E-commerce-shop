7e935fb8c1995fea47658de3419a006ad98ab136
Adrian Janula <adrian.janula@gmail.com>
2022-09-01 15:51:22 +0200

    Improve Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="sectionHome">
     <h1 class="h1">Home page</h1>
+    <p class="sectionHome__p">Welcome to the ecommerce store, which is intended to be a user friendly, 
+      fast and modern! Hope you enjoy shopping here and come back again!</p>
   </section>
 </template>
 
@@ -12,13 +14,23 @@ export default {
 <style>
 
 .sectionHome{
-  background: url(../assets/Home.jpg) no-repeat;
+  background: url(../assets/Home.jpg) no-repeat center;
   background-size: cover;
   width: 100%;
   min-height: 100vh;
+  position: relative;
 }
+
+.sectionHome__p{
+  font-size: 1.5em;
+  color: #F5DFBB;
+  margin: 0 auto;
+  padding: 100px 20px;
+  max-width: 650px;
+  background: rgba(0,0,0, .7);
+}
+
 .h1{
-   
     font-size: 3.5em;
     font-weight: bold;
     margin-top: 0;
